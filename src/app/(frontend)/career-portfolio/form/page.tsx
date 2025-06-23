afbf542daf50a203dde1968a69153314a9a2278a
Rushabh <rushabhbhalgat123@gmail.com>
2025-06-23 19:11:34 +0530

    Added the career portfolio service pages with form and payment

---
+++ src/app/(frontend)/career-portfolio/form/page.tsx
@@ -0,0 +1,8 @@
+import MyFormComponent from '@/components/MyFormComponent'
+export default async function PortfolioForm() {
+  return (
+    <>
+      <MyFormComponent formId="6859588d159e4cb70ad09ea8" />
+    </>
+  )
+}
