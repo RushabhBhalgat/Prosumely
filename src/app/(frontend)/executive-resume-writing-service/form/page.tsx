59ca0ec71b0208f4a2a96882ade4e68831544b4e
Rushabh <rushabhbhalgat123@gmail.com>
2025-06-23 17:58:11 +0530

    design improvements on forms and form message

--- src/app/(frontend)/executive-resume-writing-service/form/page.tsx
+++ src/app/(frontend)/executive-resume-writing-service/form/page.tsx
@@ -2,7 +2,6 @@ import MyFormComponent from '@/components/MyFormComponent'
 export default async function ExecutiveForm() {
   return (
     <>
-      <h1>Executive Resume Writing</h1>
       <MyFormComponent formId="685942569b739d0c37fef8b8" />
     </>
   )
--- src/app/(frontend)/visual-resume-writing-service/form/page.tsx
+++ src/app/(frontend)/visual-resume-writing-service/form/page.tsx
@@ -2,7 +2,6 @@ import MyFormComponent from '@/components/MyFormComponent'
 export default async function VisualForm() {
   return (
     <>
-      <h1>Visual Resume Writing</h1>
       <MyFormComponent formId="685941419b739d0c37fef827" />
     </>
   )
