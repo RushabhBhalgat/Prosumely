5a71e34942e9999bff74f8522f8d55b044b8624e
Rushabh <rushabhbhalgat123@gmail.com>
2025-06-23 18:09:14 +0530

    Linkedin form and payment

---
+++ src/app/(frontend)/linkedin-profile-makeover/form/page.tsx
@@ -0,0 +1,8 @@
+import MyFormComponent from '@/components/MyFormComponent'
+export default async function LinkedInForm() {
+  return (
+    <>
+      <MyFormComponent formId="68594a6e9b739d0c37fefa15" />
+    </>
+  )
+}
