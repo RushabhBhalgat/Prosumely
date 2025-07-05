56b650321354214531d1176d15a56c84acca43b3
Rushabh <rushabhbhalgat123@gmail.com>
2025-07-05 18:29:51 +0530

    created jobseekers service page and payment links

---
+++ src/app/(frontend)/jobseekers-combo-service/page.tsx
@@ -0,0 +1,8 @@
+import ServiceProductPage from '../../../components/ServiceProductPage'
+import { servicesData } from '../../../data/servicesData'
+
+const jobseekersComboPage = () => {
+  return <ServiceProductPage {...servicesData.jobseekersCombo} />
+}
+
+export default jobseekersComboPage
