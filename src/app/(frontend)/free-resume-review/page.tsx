0f14047dce8ea5c36a23f9a18d687602f2040cb3
Rushabh <rushabhbhalgat123@gmail.com>
2025-07-16 18:57:26 +0530

    fixing use-client issue on free-resume-review page

--- src/app/(frontend)/free-resume-review/page.tsx
+++ src/app/(frontend)/free-resume-review/page.tsx
@@ -22,52 +22,6 @@ import {
 } from 'lucide-react'
 import MyFormComponent from '@/components/MyFormComponent'
 
-export const metadata = {
-  title: 'Free Resume Review | Expert CV Analysis & ATS Check | Prosumely',
-  description:
-    'Get a 100% free, expert resume review from Prosumely. Instantly discover how your CV performs with ATS, keyword optimization, and recruiter insights. Boost your job search with actionable feedback—no cost, no obligation.',
-  keywords: [
-    'free resume review',
-    'free CV review',
-    'ATS resume check',
-    'resume feedback',
-    'resume analysis',
-    'Prosumely',
-    'job search',
-    'resume writing',
-    'CV optimization',
-    'career advice',
-  ],
-  openGraph: {
-    title: 'Free Resume Review | Expert CV Analysis & ATS Check | Prosumely',
-    description:
-      'Get a 100% free, expert resume review from Prosumely. Instantly discover how your CV performs with ATS, keyword optimization, and recruiter insights. Boost your job search with actionable feedback—no cost, no obligation.',
-    url: 'https://prosumely.com/free-resume-review',
-    siteName: 'Prosumely',
-    images: [
-      {
-        url: '/prosumely-Free-Resume-Review-Service-opengraph.jpg',
-        width: 1200,
-        height: 630,
-        alt: 'Prosumely Free Resume Review',
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Free Resume Review | Expert CV Analysis & ATS Check | Prosumely',
-    description:
-      'Get a 100% free, expert resume review from Prosumely. Instantly discover how your CV performs with ATS, keyword optimization, and recruiter insights. Boost your job search with actionable feedback—no cost, no obligation.',
-    images: ['/prosumely-Free-Resume-Review-Service-opengraph.jpg'],
-    site: '@prosumely',
-  },
-  alternates: {
-    canonical: 'https://prosumely.com/free-resume-review',
-  },
-}
-
 export default function FreeResumeReviewPage() {
   const formId = '685e6a9e262e346a3ebce6d6'
   const [isVisible, setIsVisible] = useState(false)
