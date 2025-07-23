94b5bb0cc7886e665926ebdf444a8dfa76d8f28c
Rushabh <rushabhbhalgat123@gmail.com>
2025-07-23 21:47:16 +0530

    fixing posts page where imagge was not loading

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -23,7 +23,12 @@ export default async function Page() {
       title: true,
       slug: true,
       categories: true,
-      meta: true,
+      meta: {
+        title: true,
+        description: true,
+        image: true,
+      },
+      heroImage: true,
     },
   })
 
